422624ca75b8a0a662cffc587c9e9c22d054bd6d
UAD\1302534 <necron_warrior@hotmail.co.uk>
2018-02-14 17:45:47 +0000

    jquery shenannigans

--- js/index.js
+++ js/index.js
@@ -4,11 +4,76 @@ $(document).ready(function() {
 
 "use strict";
 
-var msg = "hello sebi";
-console.log(msg);
+var gitHubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars"
+
+$.get(gitHubSearch)
+	.done(function(r){
+	displayResults(r.items);
+})
+	.fail(function(err){
+		console.log("Failed to connect to gitub");
+})
+	.always(function(){
+		console.log("always activated");
+});
 
 var resultList = $("#resultslist");
 resultList.text("this is jquery");
+
+// var results = [{
+	// name:"jQuery",
+	// language:"JavaScript",
+	// score: 4.5,
+	// showLog: function(){
+	
+	// },
+	// owner:{
+		// login:"necronwarrior",
+		// id:123456
+	// }
+// },{
+	// name:"jQuery UI",
+	// language:"JavaScript",
+	// score: 3.5,
+	// showLog: function(){
+	
+	// },
+	// owner:{
+		// login:"necronwarrior",
+		// id:123456
+	// }
+// }];
+	
+function displayResults(results){
+	resultList.empty();
+	$.each(results,function(i,item){
+		
+		var newResult = $("<div class='result'>" +
+		  "<div class='title'>" + item.name + "</div>" +
+		  "<div>Language: " + item.language + "</div>" +
+		  "<div>Owner: " + item.owner.login + "</div>" +
+		  "</div>");
+		  
+		newResult.css('opacity','0.0');
+		newResult.hover(function(){
+			//darken
+			$(this).animate({'opacity':'0.7'},100);
+			$(this).css("background-color","lightgray");
+		},function(){
+			//reverse
+			$(this).animate({'opacity':'0.0'},100);
+			$(this).css("background-color","transparent");
+		});
+		
+		resultList.append(newResult);
+	});
+}
+
+
+
+var msg = "hello sebi";
+console.log(msg);
+
 
 var toggleB = $("#Togglebutton");
 toggleB.on("click", function() {
@@ -16,5 +81,9 @@ resultList.toggle(500);
 if(toggleB.text() == "Hide") toggleB.text("Show");
 else toggleB.text("Hide")
 });
+
+var listitems = $("header nav li");
+listitems.css("font-weight", "bold");
+listitems.filter(":first").css("font-size","72px");
 
 });
