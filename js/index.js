873b258face29e693048dbd18c4d2cde9c8d3270
UAD\1302534 <necron_warrior@hotmail.co.uk>
2018-02-28 15:45:55 +0000

    Scrolly-images

--- js/index.js
+++ js/index.js
@@ -69,8 +69,26 @@ function displayResults(results){
 	});
 }
 
+//slidestuff
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
 
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
+
+//buttonstuff
 var msg = "hello sebi";
 console.log(msg);
 
