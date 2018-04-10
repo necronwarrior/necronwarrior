5885183f37b0af2f18b0e3520beee9b682334108
UAD\1302534 <necron_warrior@hotmail.co.uk>
2018-04-10 11:33:25 +0100

    navbar optimisation

--- js/index.js
+++ js/index.js
@@ -4,70 +4,7 @@ $(document).ready(function() {
 
 "use strict";
 
-var gitHubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars"
-
-$.get(gitHubSearch)
-	.done(function(r){
-	displayResults(r.items);
-})
-	.fail(function(err){
-		console.log("Failed to connect to gitub");
-})
-	.always(function(){
-		console.log("always activated");
-});
-
-var resultList = $("#resultslist");
-resultList.text("this is jquery");
-
-// var results = [{
-	// name:"jQuery",
-	// language:"JavaScript",
-	// score: 4.5,
-	// showLog: function(){
-	
-	// },
-	// owner:{
-		// login:"necronwarrior",
-		// id:123456
-	// }
-// },{
-	// name:"jQuery UI",
-	// language:"JavaScript",
-	// score: 3.5,
-	// showLog: function(){
-	
-	// },
-	// owner:{
-		// login:"necronwarrior",
-		// id:123456
-	// }
-// }];
-	
-function displayResults(results){
-	resultList.empty();
-	$.each(results,function(i,item){
-		
-		var newResult = $("<div class='result'>" +
-		  "<div class='title'>" + item.name + "</div>" +
-		  "<div>Language: " + item.language + "</div>" +
-		  "<div>Owner: " + item.owner.login + "</div>" +
-		  "</div>");
-		  
-		newResult.css('opacity','0.0');
-		newResult.hover(function(){
-			//darken
-			$(this).animate({'opacity':'0.7'},100);
-			$(this).css("background-color","lightgray");
-		},function(){
-			//reverse
-			$(this).animate({'opacity':'0.0'},100);
-			$(this).css("background-color","transparent");
-		});
-		
-		resultList.append(newResult);
-	});
-}
+$("#NAVFRAME").load("navbar.html");
 
 //slidestuff
 var slideIndex = 1;
@@ -95,7 +32,6 @@ document.getElementById("ButtonClickRight").onclick = function() {
 	showDivs(slideIndex += 1);
 };
 
-
 var slideIndex = 0;
 carousel();
 
@@ -111,7 +47,6 @@ function carousel() {
     setTimeout(carousel, 6000); // Change image every 2 seconds
 }
 
-//buttonstuff
 var msg = "hello sebi";
 console.log(msg);
 
